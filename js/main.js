d08b3d8789235a4cf74babafcc3e31550accaa09
Sam Cordingley <sam@cordandberg.com>
2017-12-30 17:17:17 +1100

    styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log("hello");
+
+// const winner1Screen = function () {
+//   $('#win').addClass('winOverlay');
+//   $('.winner1text').css("display", "inline-block");
+//   $('.resetButton').css("display", "inline-block");
+//   if (localStorage.winner1Screen) {
+//     localStorage.winner1Screen = Number(localStorage.winner1Screen) + 1;
+// } else {
+//     localStorage.winner1Screen = 1;
+// }
+// $('#resultPizza').css("display", "inline-block");
+// document.getElementById("resultPizza").innerHTML = "Total Wins: " +
+// localStorage.winner1Screen;
+//   resetGame();
+// }
