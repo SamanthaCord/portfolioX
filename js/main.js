0e06dfad2010e83f81be58ff73de25ef7ce93efb
Sam Cordingley <sam@cordandberg.com>
2018-01-29 23:46:15 +1100

    make contact info stick to the bottom on mobile

--- js/main.js
+++ js/main.js
@@ -73,7 +73,11 @@ $('#six').mouseleave(function() {
 
 ///////contact overlay
 
-  $("#contact").click(function() {
+  $(".contact").click(function() {
+    $("#overlay").css("display", "block");
+  })
+
+  $("#mobileContact").click(function() {
     $("#overlay").css("display", "block");
   })
 
